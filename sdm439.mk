dd1c943ed50f3750b2340f910a36eb090d28b652
skittels15 <skittels1510@mail.ru>
2021-12-28 19:39:52 +0300

    sdm439-common: Added Google Camera GO
    Update sdm439.mk

--- sdm439.mk
+++ sdm439.mk
@@ -110,17 +110,22 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+
+# Camera
+PRODUCT_PACKAGES += \
+    android.frameworks.displayservice@1.0 \
+    android.frameworks.displayservice@1.0.vendor \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service
-
-PRODUCT_PACKAGES += \
-    libstdc++.vendor
-
-PRODUCT_PACKAGES += \
-    Snap
-
-PRODUCT_PACKAGES += \
-    libwa_dof
+    android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.device@3.6 \
+    android.hardware.camera.provider@2.6 \
+    camera.msm8937 \
+    libdng_sdk.vendor \
+    libgui_vendor \
+    libmm-qcamera \
+    GCamGOPrebuilt \
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0.vendor
 
 # Display
 PRODUCT_PACKAGES += \
