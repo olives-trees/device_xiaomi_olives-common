4b5793f48cd8119c5de5043d1b5b2bcf8f6427e2
skittels15 <skittels1510@mail.ru>
2022-01-08 22:27:23 +0300

    sdm439-common: Added missed Sounds app

---
+++ GoogleSoundsPrebuilt/Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := GoogleSoundsPrebuilt
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := GoogleSoundsPrebuilt.apk
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_DEX_PREOPT := false
+include $(BUILD_PREBUILT)
