a9f4b78c9eacbde37e506f1f8c2b916e31a82652
skittels15 <skittels1510@mail.ru>
2022-01-20 01:32:37 +0300

    sdm439-common: Added Simple Gallery

--- sdm439.mk
+++ sdm439.mk
@@ -303,6 +303,10 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-service \
     libsensorndkbridge
 
+# Simple Gallery
+PRODUCT_PACKAGES += \
+    SimpleGallery
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
